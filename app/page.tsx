c56f724f3e4b6e048c3f9053263a3fc9bb926d05
PADA-1-5-04 <PADA-1-5-04@Erxess-iMac.local>
2026-01-27 15:03:34 +0800

    Ignore Next.js build output

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className = "flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to space</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl font-bold" >Welcome to space</h1>
       <Image
-        src="/images/sample.jpg"
-        alt="Sample Image"
+        src="/../public/1315743.jpg"
+        alt="Space Image"
         width={600}
         height={400}
       />

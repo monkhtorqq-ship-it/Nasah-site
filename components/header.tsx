a1d63c5328a57444832f5109dead26b6016b0d29
PADA-1-5-12 <PADA-1-5-12@Erxess-iMac.local>
2026-01-28 16:57:03 +0800

    add footer

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,6 @@
 export default function Header() {
     return (
         <header className="bg-black-500 text-white p-4 sticky top-0 w-full z-10 h-3 flex items-center align-middle">
-            <h1 className="text-xl font-bold">Welcome to Space</h1>
         </header>
     )
 }

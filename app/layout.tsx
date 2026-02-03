1ea4a2f3a982782b02cd5261761bba4032c28832
PADA-1-5-12 <PADA-1-5-12@Erxess-iMac.local>
2026-02-03 14:38:54 +0800

    Apply stashed changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
   import type { Metadata } from "next";
   import { Geist, Geist_Mono } from "next/font/google";
   import "./globals.css";
-  import Header from "./components/Header";
   import Footer from "./components/doshoo";
+import Header from "./components/Header";
 
 
   const geistSans = Geist({
